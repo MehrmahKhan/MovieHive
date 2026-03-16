0d564b839b04928d976cf2b804cc3bcb87a54262
Hafiz Abdullah <abdullakhaliq22@gmail.com>
2026-03-16 14:51:04 +0500

    Fix project setup: separate ports, wire login form, add dev script

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'login.html'));
+    res.json({
+        name: 'MovieHive API',
+        status: 'running',
+        port: PORT
+    });
 });
 
 app.post('/login', (req, res) => {
